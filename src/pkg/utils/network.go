7f8aa9b85a7136fdb9c912d6c4ba8fa9d483f9e2
Allen Conlon <allen@conlon.dev>
2026-05-01 16:12:05 -0400

    feat: fix linting
    Signed-off-by: Allen Conlon <allen@conlon.dev>

--- src/pkg/utils/network.go
+++ src/pkg/utils/network.go
@@ -103,7 +103,7 @@ func DownloadToFile(ctx context.Context, src, dst string) (err error) {
 			if config.ZarfDefaultRetries > 1 && n+1 < uint(config.ZarfDefaultRetries) {
 				l.Warn("retrying download",
 					"attempt", n+1,
-					"max_attempts", config.ZarfDefaultRetries,
+					"maxAttempts", config.ZarfDefaultRetries,
 					"url", src,
 					"error", err,
 				)
