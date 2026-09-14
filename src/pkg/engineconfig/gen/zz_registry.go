46739901c8652d9ede88e325ca987201cbb1fb30
Allen Conlon <software@conlon.dev>
2026-09-14 16:18:10 -0400

    feat(engineconfig): add k3s and rke2 v1.37 generated engine config
    Signed-off-by: Allen Conlon <software@conlon.dev>

--- src/pkg/engineconfig/gen/zz_registry.go
+++ src/pkg/engineconfig/gen/zz_registry.go
@@ -23,12 +23,14 @@ import (
 	k3s_v1_34 "github.com/colonel-byte/cargoship/src/pkg/engineconfig/gen/k3s/v1_34"
 	k3s_v1_35 "github.com/colonel-byte/cargoship/src/pkg/engineconfig/gen/k3s/v1_35"
 	k3s_v1_36 "github.com/colonel-byte/cargoship/src/pkg/engineconfig/gen/k3s/v1_36"
+	k3s_v1_37 "github.com/colonel-byte/cargoship/src/pkg/engineconfig/gen/k3s/v1_37"
 	rke2_v1_31 "github.com/colonel-byte/cargoship/src/pkg/engineconfig/gen/rke2/v1_31"
 	rke2_v1_32 "github.com/colonel-byte/cargoship/src/pkg/engineconfig/gen/rke2/v1_32"
 	rke2_v1_33 "github.com/colonel-byte/cargoship/src/pkg/engineconfig/gen/rke2/v1_33"
 	rke2_v1_34 "github.com/colonel-byte/cargoship/src/pkg/engineconfig/gen/rke2/v1_34"
 	rke2_v1_35 "github.com/colonel-byte/cargoship/src/pkg/engineconfig/gen/rke2/v1_35"
 	rke2_v1_36 "github.com/colonel-byte/cargoship/src/pkg/engineconfig/gen/rke2/v1_36"
+	rke2_v1_37 "github.com/colonel-byte/cargoship/src/pkg/engineconfig/gen/rke2/v1_37"
 )
 
 // Registry maps distro id (e.g. "k3s", "rke2") -> sanitized minor version package
@@ -41,6 +43,7 @@ var Registry = map[string]map[string]Entry{
 		"v1_34": {Server: k3s_v1_34.ServerConfig{}, Agent: k3s_v1_34.AgentConfig{}},
 		"v1_35": {Server: k3s_v1_35.ServerConfig{}, Agent: k3s_v1_35.AgentConfig{}},
 		"v1_36": {Server: k3s_v1_36.ServerConfig{}, Agent: k3s_v1_36.AgentConfig{}},
+		"v1_37": {Server: k3s_v1_37.ServerConfig{}, Agent: k3s_v1_37.AgentConfig{}},
 	},
 	"rke2": {
 		"v1_31": {Server: rke2_v1_31.ServerConfig{}, Agent: rke2_v1_31.AgentConfig{}},
@@ -49,5 +52,6 @@ var Registry = map[string]map[string]Entry{
 		"v1_34": {Server: rke2_v1_34.ServerConfig{}, Agent: rke2_v1_34.AgentConfig{}},
 		"v1_35": {Server: rke2_v1_35.ServerConfig{}, Agent: rke2_v1_35.AgentConfig{}},
 		"v1_36": {Server: rke2_v1_36.ServerConfig{}, Agent: rke2_v1_36.AgentConfig{}},
+		"v1_37": {Server: rke2_v1_37.ServerConfig{}, Agent: rke2_v1_37.AgentConfig{}},
 	},
 }
