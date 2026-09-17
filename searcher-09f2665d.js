f3c762a99cbbdeca0c26cb3b8ac45cbb598ad1f5
a1994sc <a1994sc@users.noreply.github.com>
2026-09-17 22:14:27 +0000

    deploy: d0ca61866d5b121935f46b38add40d7e978e3f78

--- searcher-09f2665d.js
+++ searcher-09f2665d.js
@@ -440,7 +440,7 @@ window.search = window.search || {};
         if (yes) {
             loadSearchScript(
                 window.path_to_searchindex_js ||
-                path_to_root + 'searchindex-7468d72d.js',
+                path_to_root + 'searchindex-c2a33b0b.js',
                 'mdbook-search-index');
             search_wrap.classList.remove('hidden');
             searchicon.setAttribute('aria-expanded', 'true');
