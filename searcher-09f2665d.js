2885833ccad56cca20016d9535400e258cab0399
a1994sc <a1994sc@users.noreply.github.com>
2026-09-22 22:09:27 +0000

    deploy: 7a96c281e27b66fa750b39348efe3d293edd0277

--- searcher-09f2665d.js
+++ searcher-09f2665d.js
@@ -440,7 +440,7 @@ window.search = window.search || {};
         if (yes) {
             loadSearchScript(
                 window.path_to_searchindex_js ||
-                path_to_root + 'searchindex-10d2060c.js',
+                path_to_root + 'searchindex-6035a989.js',
                 'mdbook-search-index');
             search_wrap.classList.remove('hidden');
             searchicon.setAttribute('aria-expanded', 'true');
